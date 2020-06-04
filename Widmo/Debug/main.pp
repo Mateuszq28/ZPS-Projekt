22260a28f298537047498a3eeb3f400010ab9fcf
Mateuszq28 <mateuszq28@gmail.com>
2020-06-04 21:08:29 +0200

    Add files via upload

---
+++ Widmo/Debug/main.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../include/dsplib.h
+main.obj: ../include/tms320.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/stdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: ../include/testsignal.h
+main.obj: ../include/hamming.h
+
+../main.c: 
+../include/dsplib.h: 
+../include/tms320.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/stdarg.h: 
+../include/testsignal.h: 
+../include/hamming.h: 
